1820f4f28efa7df06cb63125165ac6b9340b2465
Mert Sefa AKGUN <git@msakg.com>
2023-07-30 00:41:32 +0300

    refactor(routes): routes splited into routes all

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use std::net::SocketAddr;
 
 use axum::{
-    extract::Query,
+    extract::{Path, Query},
     response::{Html, IntoResponse},
     routing::get,
     Router,
@@ -12,27 +12,42 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_all = Router::new().merge(routes_hello());
 
     /* #region: Start Server */
     let addr = SocketAddr::from(([127, 0, 0, 1], 5353));
     println!("--> Server running on: http://{}/", addr);
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
     /* #endregion */
 }
+
+/* #region: Routes Hello */
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2)) // :name tells axum where to data extract from
+}
+
 #[derive(Debug, Deserialize)]
 struct HelloParams {
     name: Option<String>,
 }
-
-/* #region: Handler Hello */
+// /hello?name=msa
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("--> {:<12} - handler_hello - {params:?}", "HANDLER");
     let name = params.name.as_deref().unwrap_or("World");
 
     Html(format!("Hello <strong>{name}!</strong>"))
 }
+
+// /hello2/msa
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("--> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello2 <strong>{name}!</strong>"))
+}
+
 /* #endregion */
